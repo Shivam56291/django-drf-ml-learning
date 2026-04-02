00e3a3b4c40132a34523654dfc1e53ef05305a2c
Shivam Kumar <roysinghaniashivam@gmail.com>
2026-04-02 07:39:03 +0530

    stock-prediction-portal: Implement user authentication API with DRF - Applied migrations and created superuser; set up Django REST Framework - Created User serializer for validation and transformation - Implemented user registration API view - Configured API URLs, tested endpoints, and structured apps

---
+++ Stock-Prediction-Portal/backend-drf/accounts/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8, style={'input_type': 'password'})
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password"]
+
+    def create(self, validated_data):
+      # User.objects.create = save the password in plain text
+      # User.objects.create_user = save the password in hashed format
+      user = User.objects.create_user(**validated_data) # modern way
+        # user = User.objects.create_user(
+        #     username=validated_data["username"],
+        #     email=validated_data["email"],
+        #     password=validated_data["password"],
+        # ) # old way
+      return user
